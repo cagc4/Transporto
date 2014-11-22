06f04ef6db294b322f9e38a8b7c2cd1cbc454e5d
frajafrari <frajafrari@gmail.com>
2014-11-22 13:20:46 -0500

    prueba

--- Pages/Fuec_pri.php
+++ Pages/Fuec_pri.php
@@ -2,14 +2,15 @@
 
 include "../Clases/TemplateReport.php";
 include "../Clases/CasualTravel.php";
-
 $reporte='FUEC';
 
-$objTemplate = new TemplateReport('FUEC','P','LEGAL',false,'empleado');
+$objTemplate = new TemplateReport('FUEC','P','LEGAL',true,'empleado');
+if(!isset($_SESSION['number'])){
+	header('location:Fuec_srch.php');
+}
 $objTemplate->setupForm($reporte,false,false,false,false,false);
 
-
-$consecutivo = '376009200201400180029';
+$consecutivo = $_SESSION['number'];
 $objTemplate->fuec($consecutivo);
 $objTemplate->exportarPdf($consecutivo);
 
