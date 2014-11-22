57bdd6c1bd3de4dafb667cc96a350e25fd797b92
frajafrari <frajafrari@gmail.com>
2014-11-22 16:18:09 -0500

    Ajustes

--- Pages/Fuec_pri.php
+++ Pages/Fuec_pri.php
@@ -13,15 +13,6 @@ $objTemplate->setupForm($reporte,false,false,false,false,false);
 
 $reporte='FUEC';
 
-$objTemplate = new TemplateReport('FUEC','P','LEGAL',true,'empleado');
-
-if(!isset($_SESSION['number'])){
-	header('location:Fuec_srch.php');
-}
-
-$objTemplate->setupForm($reporte,false,false,false,false,false);
-
-
 $consecutivo = $_SESSION['number'];
 $objTemplate->fuec($consecutivo);
 $objTemplate->exportarPdf($consecutivo);
