afa71f55f92b10a3a361d2b04b8a32d0db9d41ab
Carlos Alberto Garcia Cobo <carlosgc4@gmail.com>
2015-02-09 09:20:31 -0500

    Firma como imagen

--- Clases/TemplateReport.php
+++ Clases/TemplateReport.php
@@ -1172,6 +1172,7 @@ EOD;
 		$style=$this->codigoBarraEstilo();
 		$this->pdf->write2DBarcode($numerofuec, 'QRCODE,L', 10, 10, 25,25, $style, 'N');
 		$this->pdf->Image(K_PATH_IMAGES.'supertransporte.jpg', 160,20, 40,10, '', '', '', false, 0);
+		$this->pdf->Image(K_PATH_IMAGES.'firma.png',120,192,80,25, '', '', '', false, 0);
 		$this->pdf->writeHTML($html, true, false, true, false, '');
 
 		$this->pdf->AddPage();
